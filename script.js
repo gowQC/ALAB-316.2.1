c1660c741484f59330667334d04e9893c566c124
Nicholas Osorio <nicholas.osorio98@gmail.com>
2024-11-04 14:50:44 -0500

    Added some interactivity to game

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-let correctGuess = Math.floor((Math.random() * 100) + 1);
-console.log(correctGuess);
-let maxGuesses = 10;
-let currentGuesses = 0;
+function guessingGame(num) {
+    // variables
+    let correctGuess = Math.floor((Math.random() * 100) + 1);
+    console.log(correctGuess);
+    let maxGuesses = 10;
+    let currentGuesses = 0;
 
-function guessingGame(num){
-while(currentGuesses < maxGuesses){
-    let userGuess = Number(window.prompt("Enter your guess"));
-if (userGuess === correctGuess) {
-    alert("Nice job! You guessed correctly!");
-    break;
-} else {
-    currentGuesses += 1;
-    let guessesLeft = maxGuesses - currentGuesses;
-    if (userGuess > correctGuess) {
-        alert(`Try again, it's lower than ${userGuess}. You have ${guessesLeft} guesses left!`);
-    } else if (userGuess < correctGuess) {
-        alert(`Try again, it's higher than ${userGuess}. You have ${guessesLeft} guesses left!`);
+    while (currentGuesses < maxGuesses) {
+        let guesses = document.getElementById("guesses");
+        let userGuess = Number(window.prompt("Enter your guess"));
+        if (userGuess === correctGuess) {
+            guesses.innerHTML = guesses.innerHTML + ` ${userGuess}`;
+            alert("Nice job! You guessed correctly!");
+            break;
+        } else {
+            currentGuesses += 1;
+            let guessesLeft = maxGuesses - currentGuesses;
+            guesses.innerHTML = guesses.innerHTML + ` ${userGuess}` + ",";
+            if (userGuess > correctGuess) {
+                alert(`Try again, it's lower than ${userGuess}. You have ${guessesLeft} guesses left!`);
+            } else if (userGuess < correctGuess) {
+                alert(`Try again, it's higher than ${userGuess}. You have ${guessesLeft} guesses left!`);
+            }
+        }
+        if (currentGuesses >= maxGuesses) {
+            alert(`Game over! The correct number was ${correctGuess}.`);
+            break;
+
+        }
     }
-      }
-      if (currentGuesses >= maxGuesses) {
-        alert(`Game over! The correct number was ${correctGuess}.`);
-        break;
-   
-    } 
-} }
+}
