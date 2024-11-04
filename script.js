869c25c1cc5807c20244de2c26b8c84fb76b84b3
Nicholas Osorio <nicholas.osorio98@gmail.com>
2024-11-04 15:36:19 -0500

    Added background color functionality to result + some comments.

--- script.js
+++ script.js
@@ -8,21 +8,25 @@ function guessingGame() {
     while (currentGuesses < maxGuesses) {
         let guesses = document.getElementById("guesses");
         let userGuess = Number(window.prompt("Enter your guess"));
-        if (userGuess === correctGuess) {
+        if (userGuess === correctGuess) { // correct guess
             guesses.innerHTML = guesses.innerHTML + ` ${userGuess}`;
+            guesses.style.backgroundColor = "green";
             alert("Nice job! You guessed correctly!");
             break;
-        } else {
+        } else { // if wrong with guesses left
             currentGuesses += 1;
             let guessesLeft = maxGuesses - currentGuesses;
             guesses.innerHTML = guesses.innerHTML + ` ${userGuess}` + ",";
             if (userGuess > correctGuess) {
+                guesses.style.backgroundColor = "yellow";
                 alert(`Try again, it's lower than ${userGuess}. You have ${guessesLeft} guesses left!`);
             } else if (userGuess < correctGuess) {
+                guesses.style.backgroundColor = "yellow";
                 alert(`Try again, it's higher than ${userGuess}. You have ${guessesLeft} guesses left!`);
             }
         }
-        if (currentGuesses >= maxGuesses) {
+        if (currentGuesses >= maxGuesses) { // if wrong with 0 guesses left
+            guesses.style.backgroundColor = "red";
             alert(`Game over! The correct number was ${correctGuess}.`);
             break;
         }
